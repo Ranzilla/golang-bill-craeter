a536d2063689173f742ca3a01ed75e8cbccc3112
Bitzilla <lucaraneri@hotmail.com>
2024-01-28 20:15:21 +0100

    add .gitignore

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+import "sort"
+
+func main() {
+
+// slice trough a slice with index + for loop with use of continue or break
+for index, value := range names {
+if index O == 1 {
+fmt.Printf("Continue at index %v\n", index)
+continue
+} else if {
+
+}
+
+}
+
+}
